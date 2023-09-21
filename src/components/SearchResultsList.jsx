4d07b92c9812c151eddcb8a1dd4714f05cd6e0ee
Tionlierite <Tionlierite@gmail.com>
2023-09-21 20:59:49 +0300

    feat: added search-bar with citys fetching

---
+++ src/components/SearchResultsList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {SearchResult} from "./SearchResult";
+
+import "./SearchResultsList.css"
+
+export const SearchResultsList = (results) => {
+	return (
+		<>
+			<div className="search-results">
+				<div>
+					{results.results.map((result, id) => {
+						return <SearchResult result={result.city} key={id}/>
+					})}
+				</div>
+			</div>
+		</>
+	)
+}
