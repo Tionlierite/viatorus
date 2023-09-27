efbbdd9a1c6cb5487f2eb729927c6e788879916e
Tionlierite <Tionlierite@gmail.com>
2023-09-27 18:42:43 +0300

    feat: migrate backend code from branch stash/v0.0.1

---
+++ server/index.js
@@ -0,0 +1,74 @@
+const express = require("express")
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
+
+app.get("/api", (req, res) => {
+	res.json([
+		{
+			id: 1,
+			city: "Gwenborough"
+		},
+		{
+			id: 2,
+			city: "Romaguera-Crona"
+		},
+		{
+			id: 3,
+			city: "Wisokyburgh"
+		},
+		{
+			id: 4,
+			city: "McKenziehaven"
+		},
+		{
+			id: 5,
+			city: "South Elvis"
+		},
+		{
+			id: 6,
+			city: "Roscoview"
+		},
+		{
+			id: 7,
+			city: "South Christy"
+		},
+		{
+			id: 8,
+			city: "Howemouth"
+		},
+		{
+			id: 9,
+			city: "Alliyaview"
+		},
+		{
+			id: 10,
+			city: "Bartholomobury"
+		},
+		{
+			id: 11,
+			city: "Lebsackbury"
+		},
+		{
+			id: 12,
+			city: "Penza"
+		},
+		{
+			id: 13,
+			city: "Klyazma"
+		},
+		{
+			id: 14,
+			city: "Moscow"
+		},
+		{
+			id: 15,
+			city: "Rabat"
+		}
+	])
+})
