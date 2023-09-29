b62e3d12e35f69bc38134744af4460c2c714547d
Tionlierite <Tionlierite@gmail.com>
2023-09-29 18:23:07 +0300

    Actual project moving to stash. Rebase
    Refactoring begin

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const express = require("express")
 const router = require("./routes/index")
-
+const sequelize = require("./db")
+const models = require("./models/models")
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -10,6 +12,10 @@ app.use("/api", router)
 
 async function startApp() {
 	try {
+		await sequelize.authenticate()
+		/*await sequelize.drop()
+		console.log("All tables dropped!")*/
+		await sequelize.sync()
 		app.listen(PORT, () => console.log("SERVER STARTED ON PORT " + PORT))
 	} catch (e) {
 		console.log(e)
--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express")
-const router = new Router()
-const cityRouter = require("./cityRouter")
-
-router.use("/city", cityRouter)
-
-module.exports = router
