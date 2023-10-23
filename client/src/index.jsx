8be32a795fddeb7df5dc075e51a8a6b5d00b368f
Tionlierite <Tionlierite@gmail.com>
2023-10-23 12:11:03 +0300

    [impr] Migrated from Create-React-App to Vite React
    - Added Tailwind and Prettier plugin for Tailwind

---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+// Libraries
+import React from "react"
+import ReactDOM from "react-dom/client"
+// Styles
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>Some</React.StrictMode>
+)
