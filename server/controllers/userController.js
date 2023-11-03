1794144c4e0e3349e87ebe3c66d8d1953d7c1995
Tionlierite <Tionlierite@gmail.com>
2023-11-04 01:15:23 +0300

    [backend] [feat] Added Auth middleware for auth endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -44,12 +44,10 @@ class UserController {
 		const token = generateJwt(user.user_id, user.user_email, user.user_password)
 		return res.json({ token })
 	}
-	async check(req, res, next) {
-		const { id } = req.query
-		if (!id) {
-			return next(ApiError.badRequest("Отсутствует ID"))
-		}
-		res.json(id)
+	async check(req, res) {
+		console.log(req.user)
+		const token = generateJwt(req.user.id, req.user.email, req.user.role)
+		return res.json({ token })
 	}
 }
 module.exports = new UserController()
