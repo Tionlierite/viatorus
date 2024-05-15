284ba714192ee0fe098669a192506c97f2dea136
Tionlierite <Tionlierite@gmail.com>
2024-05-15 18:46:01 +0300

    [feat | Task 21] RegisterController.php - Add JWT generation upon registration

--- server/src/infrastructure/controllers/Api/v1/RegisterController.php
+++ server/src/infrastructure/controllers/Api/v1/RegisterController.php
@@ -5,27 +5,33 @@ namespace App\infrastructure\controllers\Api\v1;
 use App\infrastructure\database\Entity\Users;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/api/v1/register', name: 'api_v1_register', methods: ['POST'])]
-    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request,
+                             UserPasswordHasherInterface $passwordHasher,
+                             EntityManagerInterface $entityManager,
+                             JWTTokenManagerInterface $jwtManager): Response
     {
         $email = $request->query->get('email');
         $password = $request->query->get('password');
         $username = $request->query->get('username');
         if (!$email || !$password || !$username) {
-            return $this->json(['message' => 'Missing required parameters'], Response::HTTP_BAD_REQUEST);
+            return $this->json(['message' => 'Missing required parameters'],
+                Response::HTTP_BAD_REQUEST);
         }
 
         $existingUser = $entityManager->getRepository(Users::class)->findOneBy(['email' => $email]);
         if ($existingUser){
-            return $this->json(['message' => 'User with this credentials already exist'], Response::HTTP_CONFLICT);
+            return $this->json(['message' => 'User with this credentials already exist'],
+                Response::HTTP_CONFLICT);
         }
 
         $user = new Users();
@@ -34,10 +40,15 @@ class RegisterController extends AbstractController
             $passwordHasher->hashPassword($user, $password)
         );
         $user->setUsername($username);
-
         $entityManager->persist($user);
         $entityManager->flush();
 
-        return $this->json(['message' => 'User created successfully'], Response::HTTP_OK);
+        $token = $jwtManager->createFromPayload($user,
+            ['expires_in' => '3600',
+            'user_id' => $user->getId()->toRfc4122(), # 01HZQTM6EY3Y23J57HN9207V99 -> 018fee3d-8585-7020-77e6-e68c66e393b2
+            'email' => $email]);
+        return $this->json(['message' => 'User created successfully',
+            'access_token' => $token],
+            Response::HTTP_CREATED);
     }
 }
