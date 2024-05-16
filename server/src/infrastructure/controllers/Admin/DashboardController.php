e6f50b08dd1fa161c2f8afb566e3bedb7a4635e2
Tionlierite <Tionlierite@gmail.com>
2024-05-16 22:33:21 +0300

    [feat | Task 24] DashboardController.php, admin.html.twig - Add dashboard page

---
+++ server/src/infrastructure/controllers/Admin/DashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\infrastructure\controllers\Admin;
+
+use App\infrastructure\database\Entity\Cities;
+use App\infrastructure\database\Entity\Countries;
+use App\infrastructure\database\Entity\Users;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('Admin/admin.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Admin Dashboard');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+    }
+}
