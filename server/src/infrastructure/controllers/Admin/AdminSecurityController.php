b1389d85ee4d10b14569a9d42939e8d54b149e79
Tionlierite <Tionlierite@gmail.com>
2024-06-12 11:05:10 +0300

    [feat | Task 31] Security.yaml, AdminSecurityController.php - Add Controller to log inside Admin dashboard

---
+++ server/src/infrastructure/controllers/Admin/AdminSecurityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\infrastructure\controllers\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminSecurityController extends AbstractController
+{
+    #[Route('/admin/login', name: 'admin_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('Admin/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+            'translation_domain' => 'admin',
+            'page_title' => 'Admin Login',
+            'csrf_token_intention' => 'authenticate',
+            'target_path' => $this->generateUrl('admin_dashboard'),
+            'username_label' => 'Email',
+            'password_label' => 'Password',
+            'sign_in_label' => 'Log in',
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
