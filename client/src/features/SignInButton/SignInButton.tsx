483639d6b5d61b8315db89c7e76a0ac7beff782c
Tionlierite <Tionlierite@gmail.com>
2024-06-14 20:10:24 +0300

    [feat | Task 30] SignInButton.tsx, SignUpButton.tsx - Add buttons for authorization

---
+++ client/src/features/SignInButton/SignInButton.tsx
@@ -0,0 +1,9 @@
+import { ButtonOutlined } from "../../shared/ui/Buttons/ButtonOutlined"
+
+export const SignInButton = () => {
+	return (
+		<>
+			<ButtonOutlined label='Sign In' />
+		</>
+	)
+}
---
+++ client/src/features/SignUpButton/SignUpButton.tsx
@@ -0,0 +1,9 @@
+import { ButtonContained } from "../../shared/ui/Buttons/ButtonContained"
+
+export const SignUpButton = () => {
+	return (
+		<>
+			<ButtonContained label='Sign Up' />
+		</>
+	)
+}
