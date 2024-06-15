5a6772538021e45ba6ca04fd741a4d1603a66107
Tionlierite <Tionlierite@gmail.com>
2024-06-15 17:10:22 +0300

    [impr | Task30] Button(Contained&Outlined).tsx - Add optional 'disabled' field

--- client/src/shared/ui/Buttons/ButtonContained/ButtonContained.tsx
+++ client/src/shared/ui/Buttons/ButtonContained/ButtonContained.tsx
@@ -1,6 +1,15 @@
-export const ButtonContained = ({ label }: { label: string }) => {
+export const ButtonContained = ({
+	label,
+	disabled
+}: {
+	label: string
+	disabled?: boolean
+}) => {
 	return (
-		<button className='bg-viat-primary text-viat-bg font-viat-body font-bold py-2 px-4 rounded-md border-2 border-viat-primary'>
+		<button
+			className='bg-viat-primary text-viat-bg font-viat-body font-bold py-2 px-4 rounded-md border-2 border-viat-primary'
+			disabled={disabled}
+		>
 			{label}
 		</button>
 	)
--- client/src/shared/ui/Buttons/ButtonOutlined/ButtonOutlined.tsx
+++ client/src/shared/ui/Buttons/ButtonOutlined/ButtonOutlined.tsx
@@ -1,6 +1,15 @@
-export const ButtonOutlined = ({ label }: { label: string }) => {
+export const ButtonOutlined = ({
+	label,
+	disabled
+}: {
+	label: string
+	disabled?: boolean
+}) => {
 	return (
-		<button className='border-2 border-viat-primary text-viat-primary font-viat-body font-bold py-2 px-4 rounded-md'>
+		<button
+			className='border-2 border-viat-primary text-viat-primary font-viat-body font-bold py-2 px-4 rounded-md'
+			disabled={disabled}
+		>
 			{label}
 		</button>
 	)
