4aa71a84a6f187cdd5e9e0919481adedb73744bc
Tionlierite <Tionlierite@gmail.com>
2024-06-17 22:42:10 +0300

    [impr | Task 30] Button(Contained&Outlined).tsx - Add interface and function as optional prop

--- client/src/shared/ui/Buttons/ButtonContained/ButtonContained.tsx
+++ client/src/shared/ui/Buttons/ButtonContained/ButtonContained.tsx
@@ -1,14 +1,19 @@
-export const ButtonContained = ({
-	label,
-	disabled
-}: {
+interface ButtonContainedProps {
 	label: string
 	disabled?: boolean
-}) => {
+	handleClick?: () => void
+}
+
+export const ButtonContained = ({
+	label,
+	disabled,
+	handleClick
+}: ButtonContainedProps) => {
 	return (
 		<button
 			className='bg-viat-primary text-viat-bg font-viat-body font-bold py-2 px-4 rounded-md border-2 border-viat-primary'
 			disabled={disabled}
+			onClick={handleClick}
 		>
 			{label}
 		</button>
