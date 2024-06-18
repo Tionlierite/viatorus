a9fc10489f34e004cdb89c1bc65c70427e55062a
Tionlierite <Tionlierite@gmail.com>
2024-06-18 13:19:24 +0300

    [feat | Task 30] SignUpButton.tsx, SignUpModalSlice.ts - Add SignUp button and slice to control Modal window

--- client/src/features/SignUpButton/SignUpButton.tsx
+++ client/src/features/SignUpButton/SignUpButton.tsx
@@ -1,9 +1,17 @@
+import { useDispatch } from "react-redux"
+
 import { ButtonContained } from "../../shared/ui/Buttons/ButtonContained"
+import { openModal } from "./model/SignUpModalSlice.ts"
 
 export const SignUpButton = () => {
+	const dispatch = useDispatch()
+
+	const handleOpenModal = () => {
+		dispatch(openModal())
+	}
 	return (
 		<>
-			<ButtonContained label='Sign Up' />
+			<ButtonContained label='Sign Up' handleClick={handleOpenModal} />
 		</>
 	)
 }
