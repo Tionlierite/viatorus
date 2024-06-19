50595b51671d9c93bd01593a81f14924832025cc
Tionlierite <Tionlierite@gmail.com>
2024-06-19 22:41:23 +0300

    [impr] http.service.ts - Add logging service to httpService

--- client/src/shared/services/http-service/http.service.ts
+++ client/src/shared/services/http-service/http.service.ts
@@ -1,13 +1,15 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios"
+import { logger } from "../logging-service"
 axios.interceptors.response.use(
 	res => res,
-	function (error) {
+	function (error: AxiosError) {
 		const expectedErrors =
 			error.response &&
 			error.response.status >= 400 &&
 			error.response.status < 500
 
 		if (!expectedErrors) {
+			logger.log(error)
 			console.log("Unexpected Error")
 		}
 		return Promise.reject(error)
