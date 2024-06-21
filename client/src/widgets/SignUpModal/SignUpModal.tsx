030513ce83145e250999179863fc97599c142271
Tionlierite <Tionlierite@gmail.com>
2024-06-21 18:27:21 +0300

    [impr] SignUpModal.tsx - Destruct SignUp modal

--- client/src/widgets/SignUpModal/SignUpModal.tsx
+++ client/src/widgets/SignUpModal/SignUpModal.tsx
@@ -1,20 +1,14 @@
 import React, { useEffect, useState } from "react"
 import { boolean, object, ref, string, ValidationError } from "yup"
+import { useDispatch, useSelector } from "react-redux"
 import { toast } from "react-toastify"
 
-import { ButtonContained } from "../../shared/ui/Buttons/ButtonContained"
-import { InputField } from "../../shared/ui/InputField"
-import { CheckField } from "../../shared/ui/CheckField"
+import { SignUpModalFields } from "../../features/SignUpModalFields"
+import { LandingPageModalView } from "../../shared/ui/Modals/LandingPageModalView"
 
-import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../../app/store/store.ts"
 import { closeModal } from "../../features/SignUpButton/model/SignUpModalSlice.ts"
 import { httpService } from "../../shared/services/http-service"
-
-// TODO: Do something with it.
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import Logo from "../../shared/media/ViatorusLogo.svg"
 
 interface formData {
 	username: string
@@ -128,71 +122,14 @@ export const SignUpModal = () => {
 	}
 
 	return (
-		<div>
-			{isModalOpen && (
-				<div className='fixed inset-0 flex items-center justify-center z-50'>
-					<div
-						className='fixed inset-0 bg-viat-bg bg-opacity-90 transition-opacity'
-						onClick={handleCloseModal}
-					></div>
-
-					<div className='relative grid grid-cols-12 gap-8 bg-white border-2 border-viat-primary rounded-md p-8 w-10/12 max-w-screen-lg'>
-						<div className='col-span-10 col-start-2'>
-							<img src={Logo} alt='Viatorus Logo' className='h-10 w-auto' />
-						</div>
-						<div className='col-span-10 col-start-2 grid grid-cols-2 gap-8'>
-							<div className='bg-viat-bg p-8 rounded-md'>
-								<form
-									className='flex flex-col space-y-4'
-									onSubmit={handleSubmit}
-								>
-									<InputField
-										label='Username'
-										name='username'
-										value={data.username}
-										error={errors.username}
-										onChange={handleChange}
-									/>
-									<InputField
-										label='Email'
-										name='email'
-										value={data.email}
-										error={errors.email}
-										onChange={handleChange}
-									/>
-									<InputField
-										label='Password'
-										name='password'
-										type='password'
-										value={data.password}
-										error={errors.password}
-										onChange={handleChange}
-									/>
-									<InputField
-										label='Repeat password'
-										name='passwordRepeat'
-										type='password'
-										value={data.passwordRepeat}
-										error={errors.passwordRepeat}
-										onChange={handleChange}
-									/>
-									<CheckField
-										label='I agree with Terms & Conditions'
-										name='terms'
-										value={data.terms}
-										error={errors.terms}
-										onChange={handleChange}
-									/>
-									<ButtonContained label='Sign Up' disabled={!isValid} />
-								</form>
-							</div>
-							<div className='flex items-center justify-center'>
-								<h2 className='text-3xl font-bold'>Come join Us</h2>
-							</div>
-						</div>
-					</div>
-				</div>
-			)}
-		</div>
+		<LandingPageModalView isOpen={isModalOpen} onClose={handleCloseModal}>
+			<SignUpModalFields
+				data={data}
+				errors={errors}
+				onChange={handleChange}
+				onSubmit={handleSubmit}
+				isValid={isValid}
+			/>
+		</LandingPageModalView>
 	)
 }
