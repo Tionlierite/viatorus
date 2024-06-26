415a9979c402a3b8a32894405c7533c6109cb567
Tionlierite <Tionlierite@gmail.com>
2024-06-26 17:56:20 +0300

    [feat | Task 35] ProfileBox.tsx - Create profile box

---
+++ client/src/features/SidebarFeatures/ProfileBox/ProfileBox.tsx
@@ -0,0 +1,26 @@
+// TODO: Do something with it.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import UserIcon from "../../../shared/media/Profile.svg"
+
+export const ProfileBox = () => {
+	return (
+		<div className='bg-viat-bg shadow-md rounded-md p-4 mt-8'>
+			<div className='flex items-center space-x-2'>
+				<img src={UserIcon} alt='User Icon' className='h-10 w-10' />
+				<div className='flex flex-col space-y-1'>
+					<div className='font-viat-small text-viat-size-body text-viat-text'>
+						{/*TODO: Make link and show username*/}
+						{"Username's workspace"}
+					</div>
+					<a
+						href=''
+						className='font-viat-small text-viat-size-small bg-viat-primary-shade text-transparent bg-clip-text'
+					>
+						Open Profile
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
