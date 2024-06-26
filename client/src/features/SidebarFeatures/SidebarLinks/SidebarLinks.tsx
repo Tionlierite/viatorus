8bcf297b2a19f28452805b1f9b515b8e8e308e13
Tionlierite <Tionlierite@gmail.com>
2024-06-26 19:15:41 +0300

    [feat | Task 35] SidebarLinks.tsx - Create sidebar links feature

---
+++ client/src/features/SidebarFeatures/SidebarLinks/SidebarLinks.tsx
@@ -0,0 +1,45 @@
+interface SidebarLink {
+	name: string
+	path: string
+	icon: {
+		img: string
+		imgAlt: string
+		altName: string
+		classes: string
+	}
+	isSelected: boolean
+}
+
+interface SidebarLinksProps {
+	linksConfig: SidebarLink[]
+}
+
+export const SidebarLinks = ({ linksConfig }: SidebarLinksProps) => {
+	const handleTextClasses = (linkIsSelected: boolean) => {
+		return linkIsSelected
+			? "text-viat-primary font-bold border-l-2 border-viat-primary pl-4"
+			: "text-viat-secondary"
+	}
+	return (
+		<div className='ml-4 mt-8 space-y-8'>
+			{linksConfig.map(link => (
+				<a
+					key={link.path}
+					href={link.path}
+					className={`flex items-center space-x-2 ${handleTextClasses(link.isSelected)}`}
+				>
+					<img
+						src={link.isSelected ? link.icon.img : link.icon.imgAlt}
+						alt={link.icon.altName}
+						className={`${link.icon.classes} ${
+							link.isSelected ? "stroke-viat-primary" : "stroke-viat-secondary"
+						}`}
+					/>
+					<span className='font-viat-body text-viat-size-body'>
+						{link.name}
+					</span>
+				</a>
+			))}
+		</div>
+	)
+}
---
+++ client/src/features/SidebarFeatures/SidebarLinks/index.ts
@@ -0,0 +1,1 @@
+export { SidebarLinks } from "./SidebarLinks.tsx"
