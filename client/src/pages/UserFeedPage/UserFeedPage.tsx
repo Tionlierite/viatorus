5b50e593aadbdf3f78379182f13b651a035df302
Tionlierite <Tionlierite@gmail.com>
2024-07-03 00:18:06 +0300

    [fix] Trimmed Feed page for refactoring

---
+++ client/src/pages/UserFeedPage/UserFeedPage.tsx
@@ -0,0 +1,9 @@
+export const UserFeedPage = () => {
+	return (
+		<main className='col-span-9'>
+			<div className='p-8'>
+				<h1 className='text-2xl font-bold mb-4'>User Feed</h1>
+			</div>
+		</main>
+	)
+}
