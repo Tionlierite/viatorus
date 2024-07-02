f4b4b13f8bdfcd27956400487fe721bf80dbfba8
Tionlierite <Tionlierite@gmail.com>
2024-07-02 22:14:10 +0300

    [feat | Task 35] AddToLisButton.tsx - Create button to open modal. Create slice

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit"
 
 import SignUpModalReducer from "../../features/SignUpButton/model/SignUpModalSlice.ts"
 import SignInModalReducer from "../../features/SignInButton/model/SignInModalSlice.ts"
+import AddToListModalReducer from "../../features/NavbarFeatures/AddToListButton/model/AddToListModalSlice.ts"
 import UserReducer from "../../entities/user/model/UserSlice.ts"
 
 export const store = configureStore({
 	reducer: {
 		SignUpModal: SignUpModalReducer,
 		SignInModal: SignInModalReducer,
+		AddToListModal: AddToListModalReducer,
 		user: UserReducer
 	}
 })
