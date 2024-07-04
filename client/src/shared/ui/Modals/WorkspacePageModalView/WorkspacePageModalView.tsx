4d02453be7f02354039af8ed3925ca2754c767be
Tionlierite <Tionlierite@gmail.com>
2024-07-04 23:15:00 +0300

    [impr] WorkspacePageModalView.tsx - Add title and exit button

--- client/src/shared/ui/Modals/WorkspacePageModalView/WorkspacePageModalView.tsx
+++ client/src/shared/ui/Modals/WorkspacePageModalView/WorkspacePageModalView.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 
+import { IconButton } from "@mui/material"
+
 interface ModalProps {
+	titleMessage: string
 	isOpen: boolean
 	onClose: () => void
 	children: React.ReactNode
 }
 
 export const WorkspacePageModalView: React.FC<ModalProps> = ({
+	titleMessage,
 	isOpen,
 	onClose,
 	children
@@ -19,7 +23,13 @@ export const WorkspacePageModalView: React.FC<ModalProps> = ({
 				className='fixed inset-0 bg-viat-bg bg-opacity-90 transition-opacity'
 				onClick={onClose}
 			/>
-			<div className='relative grid grid-cols-12 gap-8 bg-white border-2 border-viat-primary rounded-md p-8 w-10/12 max-w-screen-lg'>
+			<div className='relative grid grid-cols-12 gap-8 bg-white border-2 border-viat-primary rounded-md p-8 w-8/12 max-w-screen-md'>
+				<div className='col-span-11'>
+					<h2 className='text-2xl font-bold mb-4'>{titleMessage}</h2>
+				</div>
+				<div className='col-span-1 flex justify-end'>
+					<IconButton onClick={onClose}>X</IconButton>
+				</div>
 				{children}
 			</div>
 		</div>
