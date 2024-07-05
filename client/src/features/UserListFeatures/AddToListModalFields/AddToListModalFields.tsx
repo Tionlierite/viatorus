b41622b21c49508ecd182082b7ab429451b26d02
Tionlierite <Tionlierite@gmail.com>
2024-07-05 16:36:05 +0300

    [impr] AddToListModalFields.tsx - Refactor code, add new inputs

--- client/src/features/UserListFeatures/AddToListModalFields/AddToListModalFields.tsx
+++ client/src/features/UserListFeatures/AddToListModalFields/AddToListModalFields.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react"
-import { Autocomplete, TextField } from "@mui/material"
+import { TextField, Button } from "@mui/material"
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+import { Dayjs } from "dayjs"
 
 import { httpService } from "../../../shared/services/http-service"
+import { AutocompleteField } from "../../../shared/ui/AutocompleteField"
 
 interface Country {
 	id: number
@@ -14,78 +18,164 @@ interface City {
 	countryId: number
 }
 
+interface FieldsData {
+	countries: Country[]
+	cities: City[]
+	selectedCountry: Country | null
+	selectedCity: City | null
+	countrySearchTerm: string
+	citySearchTerm: string
+	comment: string
+	selectedDate: Dayjs | null
+	serviceLink: string
+	isServiceLinkVisible: boolean
+}
+
 export const AddToListModalFields = () => {
-	const [countries, setCountries] = useState<Country[]>([])
-	const [cities, setCities] = useState<City[]>([])
-	const [selectedCountry, setSelectedCountry] = useState<Country | null>(null)
-	const [selectedCity, setSelectedCity] = useState<City | null>(null)
+	const [data, setData] = useState<FieldsData>({
+		countries: [],
+		cities: [],
+		selectedCountry: null,
+		selectedCity: null,
+		countrySearchTerm: "",
+		citySearchTerm: "",
+		comment: "",
+		selectedDate: null,
+		serviceLink: "",
+		isServiceLinkVisible: false
+	})
 
-	const handleCountrySearch = async (searchTerm: string) => {
+	const handleChange = (target: {
+		name: string
+		value: string | Country | City | null | Dayjs | boolean
+	}) => {
+		setData(prevState => ({
+			...prevState,
+			[target.name]: target.value
+		}))
+		if (target.name === "selectedCountry") {
+			setData(prevState => ({
+				...prevState,
+				selectedCity: null,
+				cities: []
+			}))
+		}
+	}
+
+	const handleSearch = async (searchTerm: string, type: "country" | "city") => {
 		try {
-			const response = await httpService.get(`countries?search=${searchTerm}`)
-			setCountries(response.data)
-		} catch (error) {
-			console.error("Error searching countries:", error)
-		}
-	}
-
-	const handleCitySearch = async (searchTerm: string) => {
-		if (selectedCountry) {
-			try {
+			if (type === "country") {
+				const response = await httpService.get(`countries?search=${searchTerm}`)
+				setData(prevState => ({
+					...prevState,
+					countries: response.data
+				}))
+			} else if (type === "city" && data.selectedCountry) {
 				const response = await httpService.get(
-					`cities?search=${searchTerm}&countryId=${selectedCountry.id}`
+					`cities?search=${searchTerm}&countryId=${data.selectedCountry.id}`
 				)
-				setCities(response.data)
-			} catch (error) {
-				console.error("Error searching cities:", error)
+				setData(prevState => ({
+					...prevState,
+					cities: response.data
+				}))
 			}
+		} catch (error) {
+			console.error(`Error searching ${type}:`, error)
 		}
-	}
-
-	const handleSelectCountry = (country: Country | null) => {
-		setSelectedCountry(country)
-		setSelectedCity(null)
-		setCities([])
-	}
-
-	const handleSelectCity = (city: City | null) => {
-		setSelectedCity(city)
 	}
 
 	return (
 		<>
-			<div className='col-span-12'>
-				<Autocomplete
-					options={countries}
-					getOptionLabel={option => option.name}
-					renderInput={params => (
-						<TextField
-							{...params}
-							label='Country'
-							variant='outlined'
-							onChange={event => handleCountrySearch(event.target.value)}
-						/>
-					)}
-					value={selectedCountry}
-					onChange={(_, newValue) => handleSelectCountry(newValue)}
+			<div className='col-span-6'>
+				<AutocompleteField
+					options={data.countries}
+					label='Country'
+					value={data.selectedCountry}
+					searchTerm={data.countrySearchTerm}
+					onSearchTermChange={event => {
+						handleChange({
+							name: "countrySearchTerm",
+							value: event.target.value
+						})
+						handleSearch(event.target.value, "country")
+					}}
+					onValueChange={newValue =>
+						handleChange({ name: "selectedCountry", value: newValue })
+					}
+				/>
+			</div>
+			<div className='col-span-6'>
+				<AutocompleteField
+					options={data.cities}
+					label='City'
+					value={data.selectedCity}
+					searchTerm={data.citySearchTerm}
+					onSearchTermChange={event => {
+						handleChange({ name: "citySearchTerm", value: event.target.value })
+						handleSearch(event.target.value, "city")
+					}}
+					onValueChange={newValue =>
+						handleChange({ name: "selectedCity", value: newValue })
+					}
+					disabled={!data.selectedCountry}
 				/>
 			</div>
 			<div className='col-span-12'>
-				<Autocomplete
-					options={cities}
-					getOptionLabel={option => option.name}
-					renderInput={params => (
-						<TextField
-							{...params}
-							label='City'
-							variant='outlined'
-							onChange={event => handleCitySearch(event.target.value)}
-						/>
-					)}
-					value={selectedCity}
-					onChange={(_, newValue) => handleSelectCity(newValue)}
-					disabled={!selectedCountry}
+				<TextField
+					label='Comment'
+					variant='outlined'
+					multiline
+					rows={4}
+					fullWidth
+					value={data.comment}
+					onChange={event =>
+						handleChange({ name: "comment", value: event.target.value })
+					}
 				/>
+			</div>
+			<div className='col-span-12 mt-4 flex items-center space-x-4'>
+				<LocalizationProvider dateAdapter={AdapterDayjs}>
+					<DatePicker
+						label='Select Date'
+						value={data.selectedDate}
+						onChange={newValue =>
+							handleChange({ name: "selectedDate", value: newValue })
+						}
+					/>
+				</LocalizationProvider>
+				{data.isServiceLinkVisible ? (
+					<TextField
+						label='Service Link'
+						variant='outlined'
+						fullWidth
+						value={data.serviceLink}
+						onChange={event =>
+							handleChange({ name: "serviceLink", value: event.target.value })
+						}
+					/>
+				) : (
+					<Button
+						variant='contained'
+						color='primary'
+						onClick={() =>
+							handleChange({ name: "isServiceLinkVisible", value: true })
+						}
+					>
+						Add Service Link
+					</Button>
+				)}
+			</div>
+			<div className='col-span-12 mt-8 flex justify-end'>
+				<Button
+					variant='contained'
+					color='primary'
+					className='bg-viat-primary text-white font-bold py-2 px-4 rounded'
+					onClick={() => {
+						console.log(data)
+					}}
+				>
+					Send
+				</Button>
 			</div>
 		</>
 	)
